0375c9f15337c082e9144dd812df39d0bc01dcbd
Joshua Slui <joshua@dynamatrix.io>
2025-05-25 19:36:03 +0200

    Add magic 8 ball command

---
+++ modules/fun/commands.py
@@ -0,0 +1,22 @@
+import discord, random
+from discord import commands
+
+class FunCog(discord.Cog):
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
+
+    fun = commands.SlashCommandGroup("fun", "Fun commands")
+
+    @fun.command()
+    async def magic8ball(self, ctx: discord.ApplicationContext, question: str):
+        responses = [
+            "Yes", "No", "Maybe", "Ask again later", "Definitely",
+            "Absolutely not", "It is certain", "Very doubtful"
+        ]
+
+        response = random.choice(responses)
+
+        await ctx.respond(f"**To the question:** {question}\n🎱 **Magic 8-Ball says:** {response}", ephemeral=True)
+
+def setup(bot: discord.Bot):
+    bot.add_cog(FunCog(bot))
