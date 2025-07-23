a58cbcb62bfb48b1f583f089e9244351c7bf23a5
Joshua Slui <joshua@dynamatrix.io>
2025-07-23 23:49:30 +0200

    Remove unused create_or_update method from Birthday class

--- ORM/models/Birthday.py
+++ ORM/models/Birthday.py
@@ -1,18 +1,6 @@
 from . import AsyncORMBase
-from controllers.database import execute
 
 
 class Birthday(AsyncORMBase):
     table_name = "birthdays"
     pk_field = "id"  # foreign key to user id
-
-    @classmethod
-    async def create_or_update(cls, user_id, date):
-        await execute(
-            """
-            INSERT INTO birthdays (id, date) VALUES (%s, %s)
-            ON DUPLICATE KEY UPDATE date = VALUES(date), updated_at = CURRENT_TIMESTAMP
-        """,
-            [user_id, date],
-        )
-        return await cls.get(user_id)
