3ab53d362586efa8a05a75c5e496e309e20c6211
Joshua Slui <joshua@dynamatrix.io>
2025-07-23 23:50:44 +0200

    Update check_user_birthday function to handle multiple birthdays and return guild context

--- modules/checks/utility.py
+++ modules/checks/utility.py
@@ -5,10 +5,11 @@ from ORM.models.Birthday import Birthday
 
 
 async def check_user_birthday(user):
-    birthday = await Birthday.get(user.id)
-    if not birthday:
-        return
+    birthdays = await Birthday.objects.get(user.id)
+    for birthday in birthdays:
+        if not birthday:
+            return
 
-    today = datetime.now(ZoneInfo("America/New_York")).date()
-    if birthday.date.day == today.day and birthday.date.month == today.month:
-        return True
+        today = datetime.now(ZoneInfo("America/New_York")).date()
+        if birthday.date.day == today.day and birthday.date.month == today.month:
+            return True, birthday.guild
